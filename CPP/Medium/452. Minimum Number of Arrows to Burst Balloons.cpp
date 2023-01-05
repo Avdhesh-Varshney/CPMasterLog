13221531bfef2e04672bbcd8cffdd549fa3cc4e7
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-01-05 15:11:28 +0530

    Solution of Medium Level Problem

---
+++ CPP/Medium/452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,30 @@
+// Problem Link:
+// https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
+
+// Solution:
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end());
+        stack<pair<int, int>> s;
+        for(int i = points.size()-1; i >= 0; i--)
+        s.push({points[i][0], points[i][1]});
+        int count = 1;
+        int temp = s.top().second;
+        s.pop();
+        while(!s.empty()) {
+            if(s.top().first <= temp) {
+                while(!s.empty() && s.top().first <= temp) {
+                    temp = min(temp, s.top().second);
+                    s.pop();
+                }
+            }
+            else {
+                temp = s.top().second;
+                s.pop();
+                count++;
+            }
+        }
+        return count;
+    }
+};
