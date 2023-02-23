4963eae68762354804478f2966be3c7dda1e15e0
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-02-23 23:15:10 +0530

    Medium problem solutions

---
+++ CPP/Medium/167. Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,80 @@
+// Problem Link
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+// Solution:
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        int f = 0, l = target;
+        if(target > 0) {
+            while(f <= l) {
+                auto itf = find(numbers.begin(), numbers.end(), f);
+                if(itf != numbers.end()) {
+                    auto itl = find(numbers.begin(), numbers.end(), l);
+                    if(itl != numbers.end()) {
+                        if(itf == itl) {
+                            ans.push_back(abs(itf - numbers.begin()) +1);
+                            ans.push_back(abs(itl - numbers.begin()) +2);
+                        }
+                        else {
+                            ans.push_back(abs(itf - numbers.begin()) +1);
+                            ans.push_back(abs(itl - numbers.begin()) +1);
+                        }
+                        break;
+                    }
+                }
+                f++;
+                l--;
+            }
+        }
+        else if(target == 0) {
+            for(int i = 0; i < numbers.size(); i++) {
+                if(numbers[i] == target)
+                    ans.push_back(i+1);
+                if(ans.size() == 2)
+                    break;
+            }
+            if(ans.size() == 0) {
+                f++;
+                l--;
+                while(f > l) {
+                    auto itf = find(numbers.begin(), numbers.end(), f);
+                    if(itf != numbers.end()) {
+                        auto itl = find(numbers.begin(), numbers.end(), l);
+                        if(itl != numbers.end()) {
+                            ans.push_back(abs(itf - numbers.begin()) +1);
+                            ans.push_back(abs(itl - numbers.begin()) +1);
+                            break;
+                        }
+                    }
+                    f++;
+                    l--;
+                }
+            }
+        }
+        else {
+            while(f >= l) {
+                auto itf = find(numbers.begin(), numbers.end(), f);
+                if(itf != numbers.end()) {
+                    auto itl = find(numbers.begin(), numbers.end(), l);
+                    if(itl != numbers.end()) {
+                        if(itf == itl) {
+                            ans.push_back(abs(itf - numbers.begin()) +1);
+                            ans.push_back(abs(itl - numbers.begin()) +2);
+                        }
+                        else {
+                            ans.push_back(abs(itf - numbers.begin()) +1);
+                            ans.push_back(abs(itl - numbers.begin()) +1);
+                        }
+                        break;
+                    }
+                }
+                f--;
+                l++;
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
