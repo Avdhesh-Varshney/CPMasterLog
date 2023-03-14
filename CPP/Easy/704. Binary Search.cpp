63635d1e58bf1185637453b2cc65a27e504838c8
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-03-14 11:12:02 +0530

    Algorithms Based Problems
    Question and Solution of Algorithms Plan on LeetCode.
    (Algorithm - I)

---
+++ CPP/Easy/704. Binary Search.cpp
@@ -0,0 +1,22 @@
+// Problem Link
+// https://leetcode.com/problems/binary-search/
+
+// Solution:
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int st = 0, lt = nums.size()-1, mid = (st+lt)/2, ans = -1;
+        while(st <= lt) {
+            if(nums[mid] == target) {
+                ans = mid;
+                break;
+            }
+            else if(nums[mid] < target)
+                st = mid+1;
+            else if(nums[mid] > target)
+                lt = mid-1;
+            mid = (st+lt)/2;
+        }
+        return ans;
+    }
+};
