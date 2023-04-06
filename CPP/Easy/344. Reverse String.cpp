c2a2cb3a49389ec72cef7157bf44348768653a31
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-04-06 17:54:11 +0530

    String problems
    Best solution of string problems.

---
+++ CPP/Easy/344. Reverse String.cpp
@@ -0,0 +1,14 @@
+// Problem Link
+// https://leetcode.com/problems/reverse-string/
+
+// Solution:
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i = 0; i < s.size()/2; i++) {
+            char ch = s[i];
+            s[i] = s[s.size()-1-i];
+            s[s.size()-1-i] = ch;
+        }
+    }
+};
