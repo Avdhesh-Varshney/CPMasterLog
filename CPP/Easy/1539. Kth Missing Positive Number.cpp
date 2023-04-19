30aa8ce5dac940d04b94c7de2613b8ba4cbb0c18
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-04-19 19:51:24 +0530

    Array Based problems
    Easy and Medium level problems on the basis of array and small Maths calculation is applied here.

---
+++ CPP/Easy/1539. Kth Missing Positive Number.cpp
@@ -0,0 +1,15 @@
+// Problem Link
+// https://leetcode.com/problems/kth-missing-positive-number/
+
+// Solution:
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int i = 1;
+        while(k > 0) {
+            if(find(arr.begin(), arr.end(), i) == arr.end()) k--;
+            i++;
+        }
+        return --i;
+    }
+};
