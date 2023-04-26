c22ef28b4b2cc9bb0ec6f49317e3238874cfc200
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-04-26 19:44:04 +0530

    Tricky problems
    Best Hard & Medium level problems with different twist in the problem showing different concepts of the DSA.

---
+++ POTD/Medium/Maximum Intersecting Lines.cpp
@@ -0,0 +1,39 @@
+// 10 April 2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int maxIntersections(vector<vector<int>> lines, int N) {
+        // Code here
+        map<int, int> mp;
+        for(auto &i : lines) {
+            int u = i[0], v = i[1] + 1;
+            mp[u]++, mp[v]--;
+        }
+        int ans = 0, sum = 0;
+        for(auto &i : mp) {
+            sum += i.second;
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> mat(N, vector<int>(2));
+        for(int j = 0; j < 2; j++) {
+            for(int i = 0; i < N; i++)
+                cin >> mat[i][j];
+        }
+        Solution obj;
+        cout << obj.maxIntersections(mat, N) << endl;
+    }
+    return 0;
+}
