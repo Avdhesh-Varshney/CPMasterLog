227acb257feece19f713e48c4717d7921a9125dd
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-05-10 11:04:52 +0530

    Strings Problems
    Easiest problems of Strings on Codeforces platform

--- Strings/A_Is_It_a_Cat.cpp
+++ Strings/A_Is_It_a_Cat.cpp
@@ -1,3 +1,5 @@
+// Way - 1
+
 #include <bits/stdc++.h>
 using namespace std;
 bool check(string &s, int n) {
@@ -37,6 +39,47 @@ int main() {
             cout << "YES" << endl;
         else
             cout << "NO" << endl;
+    }
+    return 0;
+}
+
+// Way - 2
+#include <bits/stdc++.h>
+using namespace std;
+bool verify(int &n, string &s) {
+    long long i = 0, flag = 0;
+    while(i < n && s[i] == 'm') {
+        flag = 1;
+        i++;
+    }
+    if(flag != 1) return false;
+    while(i < n && s[i] == 'e') {
+        flag = 2;
+        i++;
+    }
+    if(flag != 2) return false;
+    while(i < n && s[i] == 'o') {
+        flag = 3;
+        i++;
+    }
+    if(flag != 3) return false;
+    while(i < n && s[i] == 'w') {
+        flag = 4;
+        i++;
+    }
+    if(flag != 4 || i < n) return false;
+    return true;
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        if(verify(n, s)) cout << "YES" << endl;
+        else cout << "NO" << endl;
     }
     return 0;
 }
