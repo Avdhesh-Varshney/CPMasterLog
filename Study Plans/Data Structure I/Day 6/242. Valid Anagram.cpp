ab6980e8b119eb3a323e45102e1ce81fd8b08445
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-05-18 07:01:19 +0530

    Day 6
    Day 6 done of Data Structure I Plan

---
+++ Study Plans/Data Structure I/Day 6/242. Valid Anagram.cpp
@@ -0,0 +1,17 @@
+// Problem Link
+// https://leetcode.com/problems/valid-anagram/
+
+// Solution:
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i = 0; i < s.size(); i++)
+            if(s[i] != t[i])
+                return false;
+        return true;
+    }
+};
